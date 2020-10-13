c1305a9096ecddbf689cc158622f55984abc247f
Satheesh K <javish.fri@gmail.com>
2020-10-13 23:03:40 +0530

    UnitTestcase added for viewcontroller

---
+++ TechMahendraTests/ViewControllerTests.swift
@@ -0,0 +1,44 @@
+//
+//  ViewControllerTests.swift
+//  TechMahendraTests
+//
+//  Created by Satheesh Speed Mac on 13/10/20.
+//
+
+import XCTest
+@testable import TechMahendra
+
+class ViewControllerTests: XCTestCase {
+
+    let viewController = ViewController()
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testLayout() throws {
+        viewController.setUpUILayout()
+    }
+    
+    func testViewModelCall() throws {
+        viewController.callToViewModelForUIUpdate()
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
